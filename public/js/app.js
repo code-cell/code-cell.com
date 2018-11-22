01bcc7f1588a5a0f491a0e8edace1b3426426b28
Albert Callarisa <albert@acroca.com>
2018-11-22 09:22:26 +0100

    legal stuff

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,23 @@
 $(document).foundation()
+
+cookieconsent.initialise({
+  content: {
+    header: 'Cookies usadas en ésta pagina!',
+    message: 'Este sitio web utiliza cookies para que usted tenga la mejor experiencia de usuario. Si continúa navegando está dando su consentimiento para la aceptación de las mencionadas cookies y la aceptación de nuestra política de cookies.',
+    dismiss: 'Entendido!',
+    link: 'Más información',
+    href: 'https://www.code-cell.com/politica-cookies',
+    close: '&#x274c;',
+    policy: 'Política de cookies',
+    target: '_blank',
+  },
+  palette:{
+    popup: {background: "#fff"},
+    button: {background: "#aa0000"},
+  },
+  type: 'info',
+  law: {
+    regionalLaw: false,
+  },
+  location: true,
+});
